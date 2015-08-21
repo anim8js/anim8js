80a3a17d76df51d0faf3c92e28f318eedd239d32
Philip Diffenderfer <pdiffenderfer@gmail.com>
2015-08-21 13:13:36 -0400

    Fixed dynamic composite attributes on PIXI animators

--- plugins/anim8js-pixi.js
+++ plugins/anim8js-pixi.js
@@ -93,6 +93,47 @@ anim8.override( anim8.AnimatorPixi.prototype = new anim8.Animator(),
 
     return this;
   },
+  preupdate: function(now)
+  {
+    // If there are attribute placed on the animator since the last preupdate
+    // that has computed values we need to replace the path on the event with
+    // a copy containing the computed values. This is where current value & 
+    // relative values are injected from the animator into the attribute.
+    var aa = this.attrimatorsAdded;
+    if ( aa.length )
+    {
+      for (var i = 0; i < aa.length; i++)
+      {
+        var attrimator = aa[ i ];
+        var attr = attrimator.attribute;
+        var dynamic = this.dynamics.get( attr );
+
+        if ( attr in this.subject )
+        {
+          if ( dynamic )
+          {
+            this.setDefault( attr ); // TODO
+          }
+          else
+          {
+            this.frame[ attr ] = this.subject[ attr ];            
+          }
+        }
+        else
+        {
+          this.setDefault( attr );
+        }
+
+        attrimator.start( now, this );     
+      }
+
+      aa.length = 0;
+    }
+
+    this.trigger('preupdate');
+    
+    return this;
+  },
   apply: function()
   {
     // If a dynamic attribute has been updated, call the dynamic function and
