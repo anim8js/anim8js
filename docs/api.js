9b4c30e72600d674425eef1d46ac960be1a8a38a
Philip Diffenderfer <pdiffenderfer@gmail.com>
2016-08-28 01:33:43 -0400

    Docs & Examples

---
+++ docs/api.js
@@ -0,0 +1,27 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "anim8",
+        "anim8.Animation",
+        "anim8.Animator",
+        "anim8.Animators",
+        "anim8.Attrimator",
+        "anim8.AttrimatorMap",
+        "anim8.DeferAnimator",
+        "anim8.Path",
+        "anim8.Tween",
+        "anim8.defaults",
+        "anim8.delegate",
+        "anim8.eventize"
+    ],
+    "modules": [
+        "anim8"
+    ],
+    "allModules": [
+        {
+            "displayName": "anim8",
+            "name": "anim8"
+        }
+    ]
+} };
+});
