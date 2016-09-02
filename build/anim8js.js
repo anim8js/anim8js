83acb3ed4504bd4ee80ff92d9d97c057259eee4f
Philip Diffenderfer <pdiffenderfer@gmail.com>
2016-09-01 22:16:02 -0400

    PathDelta & PathParametric needed to be defined before the child classes

--- build/anim8js.js
+++ build/anim8js.js
@@ -10680,6 +10680,161 @@ Class.extend( BuilderTweenTo, Builder,
 
 
 /**
+ * Instantiates a new PathDelta.
+ *
+ * @param {String|false} name
+ * @param {Calculator} calculator
+ * @param {Array} points
+ * @param {Array} deltas
+ * @class PathDelta
+ * @constructor
+ * @extends Path
+ */
+function PathDelta(name, calculator, points, deltas)
+{
+  this.name = name;
+  this.set( calculator, points, deltas );
+}
+
+Class.extend( PathDelta, Path,
+{
+  set: function(calculator, points, deltas)
+  {
+    this.reset( calculator, points );
+    this.deltas = deltas;
+  },
+
+  compute: function(out, delta)
+  {
+    var ds = this.deltas;
+    var end = ds.length - 2;
+    var i = 0;
+    while (ds[i + 1] < delta && i < end) {
+      i++;
+    }
+    var d0 = ds[i];
+    var d1 = ds[i + 1];
+    var pd = (delta - d0) / (d1 - d0);
+    var p0 = this.resolvePoint( i );
+    var p1 = this.resolvePoint( i + 1 );
+
+    return this.calculator.interpolate( out, p0, p1, pd );
+  },
+
+  copy: function()
+  {
+    return new PathDelta( this.name, this.calculator, copy(this.points), copy(this.deltas) );
+  }
+});
+
+
+/**
+ * Instantiates a new PathParametric.
+ *
+ * @param {String|false} name
+ * @param {Calculator} calculator
+ * @param {Array} points
+ * @param {Boolean} loop
+ * @param {Array} matrix
+ * @param {Number} weight
+ * @class PathParametric
+ * @constructor
+ * @extends Path
+ */
+function PathParametric(name, calculator, points, loop, matrix, weight)
+{
+  this.name = name;
+  this.set( calculator, points, loop, matrix, weight );
+}
+
+Class.extend( PathParametric, Path,
+{
+  set: function(calculator, points, loop, matrix, weight)
+  {
+    this.reset( calculator, points );
+    this.loop = loop;
+    this.matrix = matrix;
+    this.weight = weight;
+    this.temp = calculator.create();
+  },
+
+  compute: function(out, delta)
+  {
+    var calc = this.calculator;
+    var temp = this.temp;
+    var matrix = this.matrix;
+    var n = this.points.length;
+    var a = delta * n;
+    var i = clamp( Math.floor( a ), 0, n - 1 );
+    var d = a - i;
+
+    var p0 = this.resolvePoint( i - 1 );
+    var p1 = this.resolvePoint( i );
+    var p2 = this.resolvePoint( i + 1 );
+    var p3 = this.resolvePoint( i + 2 );
+
+    var d0 = 1;
+    var d1 = d;
+    var d2 = d * d1;
+    var d3 = d * d2;
+
+    out = calc.zero( out );
+
+    temp = calc.zero( temp );
+    temp = calc.adds( temp, p0, matrix[0][0] );
+    temp = calc.adds( temp, p1, matrix[0][1] );
+    temp = calc.adds( temp, p2, matrix[0][2] );
+    temp = calc.adds( temp, p3, matrix[0][3] );
+    out = calc.adds( out, temp, d0 );
+
+    temp = calc.zero( temp );
+    temp = calc.adds( temp, p0, matrix[1][0] );
+    temp = calc.adds( temp, p1, matrix[1][1] );
+    temp = calc.adds( temp, p2, matrix[1][2] );
+    temp = calc.adds( temp, p3, matrix[1][3] );
+    out = calc.adds( out, temp, d1 );
+
+    temp = calc.zero( temp );
+    temp = calc.adds( temp, p0, matrix[2][0] );
+    temp = calc.adds( temp, p1, matrix[2][1] );
+    temp = calc.adds( temp, p2, matrix[2][2] );
+    temp = calc.adds( temp, p3, matrix[2][3] );
+    out = calc.adds( out, temp, d2 );
+
+    temp = calc.zero( temp );
+    temp = calc.adds( temp, p0, matrix[3][0] );
+    temp = calc.adds( temp, p1, matrix[3][1] );
+    temp = calc.adds( temp, p2, matrix[3][2] );
+    temp = calc.adds( temp, p3, matrix[3][3] );
+    out = calc.adds( out, temp, d3 );
+
+    out = calc.scale( out, this.weight );
+
+    return out;
+  },
+
+  resolvePoint: function(i)
+  {
+    var points = this.points;
+    var n = points.length;
+    var k = (this.loops ? (i + n) % n : clamp( i, 0, n - 1 ));
+
+    return resolve( points[ k ] );
+  },
+
+  copy: function()
+  {
+    return new PathParametric( this.name, this.calculator, copy(this.points), this.loop, this.matrix, this.weight );
+  },
+
+  isLinear: function()
+  {
+    return false;
+  }
+});
+
+
+/**
  * Instantiates a new PathBasisSpline.
  *
  * @param {String|false} name
@@ -11045,55 +11200,6 @@ Class.extend( PathCubic, Path,
 
 
 /**
- * Instantiates a new PathDelta.
- *
- * @param {String|false} name
- * @param {Calculator} calculator
- * @param {Array} points
- * @param {Array} deltas
- * @class PathDelta
- * @constructor
- * @extends Path
- */
-function PathDelta(name, calculator, points, deltas)
-{
-  this.name = name;
-  this.set( calculator, points, deltas );
-}
-
-Class.extend( PathDelta, Path,
-{
-  set: function(calculator, points, deltas)
-  {
-    this.reset( calculator, points );
-    this.deltas = deltas;
-  },
-
-  compute: function(out, delta)
-  {
-    var ds = this.deltas;
-    var end = ds.length - 2;
-    var i = 0;
-    while (ds[i + 1] < delta && i < end) {
-      i++;
-    }
-    var d0 = ds[i];
-    var d1 = ds[i + 1];
-    var pd = (delta - d0) / (d1 - d0);
-    var p0 = this.resolvePoint( i );
-    var p1 = this.resolvePoint( i + 1 );
-
-    return this.calculator.interpolate( out, p0, p1, pd );
-  },
-
-  copy: function()
-  {
-    return new PathDelta( this.name, this.calculator, copy(this.points), copy(this.deltas) );
-  }
-});
-
-
-/**
  * Instantiates a new PathHermite.
  *
  * @param {String|false} name
@@ -11300,112 +11406,6 @@ PathLinear.getTimes = function(calc, points)
 
 	return distances;
 };
-
-
-/**
- * Instantiates a new PathParametric.
- *
- * @param {String|false} name
- * @param {Calculator} calculator
- * @param {Array} points
- * @param {Boolean} loop
- * @param {Array} matrix
- * @param {Number} weight
- * @class PathParametric
- * @constructor
- * @extends Path
- */
-function PathParametric(name, calculator, points, loop, matrix, weight)
-{
-  this.name = name;
-  this.set( calculator, points, loop, matrix, weight );
-}
-
-Class.extend( PathParametric, Path,
-{
-  set: function(calculator, points, loop, matrix, weight)
-  {
-    this.reset( calculator, points );
-    this.loop = loop;
-    this.matrix = matrix;
-    this.weight = weight;
-    this.temp = calculator.create();
-  },
-
-  compute: function(out, delta)
-  {
-    var calc = this.calculator;
-    var temp = this.temp;
-    var matrix = this.matrix;
-    var n = this.points.length;
-    var a = delta * n;
-    var i = clamp( Math.floor( a ), 0, n - 1 );
-    var d = a - i;
-
-    var p0 = this.resolvePoint( i - 1 );
-    var p1 = this.resolvePoint( i );
-    var p2 = this.resolvePoint( i + 1 );
-    var p3 = this.resolvePoint( i + 2 );
-
-    var d0 = 1;
-    var d1 = d;
-    var d2 = d * d1;
-    var d3 = d * d2;
-
-    out = calc.zero( out );
-
-    temp = calc.zero( temp );
-    temp = calc.adds( temp, p0, matrix[0][0] );
-    temp = calc.adds( temp, p1, matrix[0][1] );
-    temp = calc.adds( temp, p2, matrix[0][2] );
-    temp = calc.adds( temp, p3, matrix[0][3] );
-    out = calc.adds( out, temp, d0 );
-
-    temp = calc.zero( temp );
-    temp = calc.adds( temp, p0, matrix[1][0] );
-    temp = calc.adds( temp, p1, matrix[1][1] );
-    temp = calc.adds( temp, p2, matrix[1][2] );
-    temp = calc.adds( temp, p3, matrix[1][3] );
-    out = calc.adds( out, temp, d1 );
-
-    temp = calc.zero( temp );
-    temp = calc.adds( temp, p0, matrix[2][0] );
-    temp = calc.adds( temp, p1, matrix[2][1] );
-    temp = calc.adds( temp, p2, matrix[2][2] );
-    temp = calc.adds( temp, p3, matrix[2][3] );
-    out = calc.adds( out, temp, d2 );
-
-    temp = calc.zero( temp );
-    temp = calc.adds( temp, p0, matrix[3][0] );
-    temp = calc.adds( temp, p1, matrix[3][1] );
-    temp = calc.adds( temp, p2, matrix[3][2] );
-    temp = calc.adds( temp, p3, matrix[3][3] );
-    out = calc.adds( out, temp, d3 );
-
-    out = calc.scale( out, this.weight );
-
-    return out;
-  },
-
-  resolvePoint: function(i)
-  {
-    var points = this.points;
-    var n = points.length;
-    var k = (this.loops ? (i + n) % n : clamp( i, 0, n - 1 ));
-
-    return resolve( points[ k ] );
-  },
-
-  copy: function()
-  {
-    return new PathParametric( this.name, this.calculator, copy(this.points), this.loop, this.matrix, this.weight );
-  },
-
-  isLinear: function()
-  {
-    return false;
-  }
-});
 
 
 /**
